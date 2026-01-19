0e0389f7a346f48d73a95da646480c0cd8017ebc
HULYO193 <114851079+HULYO193@users.noreply.github.com>
2026-01-19 19:13:51 +0200

    Initial commit

--- WebsiteJS.js
+++ WebsiteJS.js
@@ -9,7 +9,7 @@ const profileData = {
 const links = [
   {
     title: "לחצו כאן כדי להצטרף לקהילת וואטסאפ",
-    url: "https://chat.whatsapp.com/JQpsOHtY33fLm47d4LKccb",
+    url: "https://chat.whatsapp.com/HyiLqPzZQDbExlIO9y2EaE", //new link
     icon: "❤️",
     isHighlighted: true,
   },
@@ -74,3 +74,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Add smooth scroll behavior
 document.documentElement.style.scrollBehavior = "smooth";
+
